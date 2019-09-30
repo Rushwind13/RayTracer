30f62aef0eb95b5945602dcc0b6a85826ed63e9f
Jimbo S. Harris <jimbo.harris@concur.com>
2019-09-30 16:42:06 -0700

    make recursion overflows more obvious

--- IntersectWith/src/IntersectWith.cpp
+++ IntersectWith/src/IntersectWith.cpp
@@ -49,7 +49,8 @@ bool IntersectWith::local_work(msgpack::sbuffer *header, msgpack::sbuffer *paylo
     if(pixel.depth >= world.maxdepth*2)
     {
         std::cout << "(" << std::setw(3) << pixel.x << "," << std::setw(3) << pixel.y << ")" << pixel.type << " exceeds maxdepth; no further intersection allowed." << std::endl;
-        return false;
+				pixel.color = glm::vec3(0.1, 1.0, 1.0);
+        return true;
     }
 
 
