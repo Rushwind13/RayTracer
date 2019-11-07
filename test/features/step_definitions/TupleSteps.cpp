ef694358aab565982de2304938c80c1ba971414a
Jimbo S. Harris <jimbo@loiosh>
2019-11-06 20:40:55 -0800

    distinguishing vectors from positions

--- test/features/step_definitions/TupleSteps.cpp
+++ test/features/step_definitions/TupleSteps.cpp
@@ -7,26 +7,36 @@ using cucumber::ScenarioScope;
 struct TupleCtx
 {
   glm::vec3 vec;
-  glm::vec3 pos;
+  glm::vec3 vec_b;
+  Position pos;
   glm::vec3 result;
 };
 
-GIVEN("^I have a position 1,0,0 in the data$")
+GIVEN("^I have a position ([0-9.]+),([0-9.]+),([0-9.]+) in the data$")
 {
-  // REGEX_PARAM(double,x);
-  // REGEX_PARAM(double,y);
-  // REGEX_PARAM(double,z);
+  REGEX_PARAM(double,x);
+  REGEX_PARAM(double,y);
+  REGEX_PARAM(double,z);
   ScenarioScope<TupleCtx> context;
-  context->pos = glm::vec3(1,0,0);
+  context->pos = Position(x,y,z);
 }
 
-GIVEN("^I have a vector 0,1,0 in the data$")
+GIVEN("^I have a vector ([0-9.]+),([0-9.]+),([0-9.]+) in the data$")
 {
-  // REGEX_PARAM(double,x);
-  // REGEX_PARAM(double,y);
-  // REGEX_PARAM(double,z);
+  REGEX_PARAM(double,x);
+  REGEX_PARAM(double,y);
+  REGEX_PARAM(double,z);
   ScenarioScope<TupleCtx> context;
-  context->vec = glm::vec3(0,1,0);
+  context->vec = glm::vec3(x,y,z);
+}
+
+GIVEN("^I have a second vector ([0-9.]+),([0-9.]+),([0-9.]+) in the data$")
+{
+  REGEX_PARAM(double,x);
+  REGEX_PARAM(double,y);
+  REGEX_PARAM(double,z);
+  ScenarioScope<TupleCtx> context;
+  context->vec_b = glm::vec3(x,y,z);
 }
 
 WHEN("^I press add$")
@@ -36,12 +46,29 @@ WHEN("^I press add$")
   context->result = context->pos + context->vec;
 }
 
-THEN("^the result should be 1,1,0 on the screen$")
+WHEN("^I press add_vector$")
 {
-  // REGEX_PARAM(double,x);
-  // REGEX_PARAM(double,y);
-  // REGEX_PARAM(double,z);
   ScenarioScope<TupleCtx> context;
 
-  EXPECT_EQ(glm::vec3(1,1,0), context->result);
+  context->result = context->vec_b + context->vec;
+}
+
+THEN("^the result should be ([0-9.]+),([0-9.]+),([0-9.]+) a position$")
+{
+  REGEX_PARAM(double,x);
+  REGEX_PARAM(double,y);
+  REGEX_PARAM(double,z);
+  ScenarioScope<TupleCtx> context;
+
+  EXPECT_EQ(Position(x,y,z), context->result);
+}
+
+THEN("^the result should be ([0-9.]+),([0-9.]+),([0-9.]+) a vector$")
+{
+  REGEX_PARAM(double,x);
+  REGEX_PARAM(double,y);
+  REGEX_PARAM(double,z);
+  ScenarioScope<TupleCtx> context;
+
+  EXPECT_EQ(glm::vec3(x,y,z), context->result);
 }
--- test/features/tuples.feature
+++ test/features/tuples.feature
@@ -4,8 +4,14 @@ Feature: Tuples
   As a ray tracer
   I want to test many things about vector math
 
-  Scenario: add vector to position
-    Given I have a position 1,0,0 in the data
-    And I have a vector 0,1,0 in the data
-    When I press add
-    Then the result should be 1,1,0 on the screen
+    Scenario: add vector to position
+      Given I have a position 1,0,0 in the data
+      And I have a vector 0,1,0 in the data
+      When I press add
+      Then the result should be 1,1,0 a position
+
+    Scenario: add vector to vector
+      Given I have a vector 1,0,0 in the data
+      And I have a second vector 0,1,0 in the data
+      When I press add_vector
+      Then the result should be 1,1,0 a vector
