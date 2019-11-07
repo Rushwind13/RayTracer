b930a511a16c6c873f75ebe5f2da4bb3b2080532
Jimbo S. Harris <jimbo@loiosh>
2019-11-06 20:11:56 -0800

    output to bin

--- test/build.sh
+++ test/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x
-gcc -o features/step_definitions/TupleSteps features/step_definitions/TupleSteps.cpp -L/usr/local/lib -lcucumber-cpp -lc++ -lboost_program_options -lboost_regex -lboost_filesystem -lboost_system -lgtest -I../libRaytracer2014/src -std=c++11
+gcc -o bin/TupleSteps features/step_definitions/TupleSteps.cpp -L/usr/local/lib -lcucumber-cpp -lc++ -lboost_program_options -lboost_regex -lboost_filesystem -lboost_system -lgtest -I../libRaytracer2014/src -std=c++11
