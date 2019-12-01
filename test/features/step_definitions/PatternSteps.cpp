586a7e56812a0e652b0463b7dfbc14f73e12a8be
Deb von Seggern <deb.von.seggern@sap.com>
2019-12-01 13:07:16 -0800

    patterns tests not working yet, Ring roughed in

---
+++ test/features/step_definitions/PatternSteps.cpp
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+#include <cucumber-cpp/autodetect.hpp>
+
+#include <Math.hpp>
+using cucumber::ScenarioScope;
+#include "TestContext.hpp"
+
+
+/*#######
+##
+## GIVEN
+##
+#######*/
+GIVEN("^Black is the color 0.1,0.1,0.1$")
+{
+    
+}
+
+/*#######
+##
+## WHEN
+##
+#######*/
+
+
+/*#######
+##
+## THEN
+##
+#######*/
