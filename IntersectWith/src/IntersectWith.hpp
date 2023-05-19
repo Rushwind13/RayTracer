2de0ea60e2072fa24254456080a67166aaee2b3a
Jimbo S. Harris <jimbo@loiosh>
2023-05-19 13:35:35 -0700

    IntersectWith now passes along EOFs received (including fake Intersection payload) and maintains pixel_count

--- IntersectWith/src/IntersectWith.hpp
+++ IntersectWith/src/IntersectWith.hpp
@@ -15,7 +15,7 @@ class IntersectWith : public Widget
 {
 public:
 	IntersectWith( char *_name, char *_subscription, char *_sub_endpoint, char *_publication, char *_pub_endpoint ) :
-		Widget( _name, _subscription, _sub_endpoint, _publication, _pub_endpoint, true, true )
+		Widget( _name, _subscription, _sub_endpoint, _publication, _pub_endpoint, true, false )
 	{
 	};
 	~IntersectWith(){};
@@ -29,6 +29,7 @@ protected:
 private:
 	Object *object;
 	World world;
+    int pixel_count;
 };
 
 
